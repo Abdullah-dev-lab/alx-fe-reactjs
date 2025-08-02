152ca16036a0170fbae2de16b7c86828dc87adc4
Abdulmalik Abdullah <abdullahtemitope1@gmail.com>
2025-08-02 13:04:15 +0100

    Create a Recipe Sharing App

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import './App.css'
+import { useRecipeStore } from './recipeStore';
+import RecipeList from './components/RecipeList';
+import AddRecipeForm from './components/AddRecipeForm';
+
+function App() {
+  const recipes = useRecipeStore((state) => state.recipes);
+  const addRecipe = useRecipeStore((state) => state.addRecipe);
+  const initializeRecipes = useRecipeStore((state) => state.initializeRecipes);
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <h1>My Recipe App</h1>
+      <AddRecipeForm onAddRecipe={addRecipe} />
+      <RecipeList recipes={recipes} />
+    </div>
+  );
+}
+
+export default App;
