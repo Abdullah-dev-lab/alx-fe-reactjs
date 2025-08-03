688c42695083c1f3b96dce7f7feac5c774622744
Abdulmalik Abdullah <abdullahtemitope1@gmail.com>
2025-08-03 15:54:05 +0100

    add searchba component

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,22 +1,27 @@
 import { useRecipeStore } from './recipeStore';
 import { Link } from 'react-router-dom';
 import DeleteRecipeButton from './DeleteRecipeButton';
+import SearchBar from './SearchBar';
 
 function RecipeList() {
-  const recipes = useRecipeStore(state => state.recipes);
-
+  const filteredRecipes = useRecipeStore(state => state.recipes);
+ 
   return (
-    <ul>
-      {recipes.map((recipe) => (
-        <li key={recipe.id}>
-          <Link to={`/recipe/${recipe.id}`}>{recipe.title}</Link>
-          {' | '}
-          <Link to={`/recipe/${recipe.id}/edit`}>Edit</Link>
-          {' | '}
-          <DeleteRecipeButton id={recipe.id} />
-        </li>
-      ))}
-    </ul>
+   <div>
+      <SearchBar />
+      <ul>
+          {filteredRecipes.map((recipe) => (
+            <li key={recipe.id}>
+              <Link to={`/recipe/${recipe.id}`}>{recipe.title}</Link>
+              {' | '}
+              <Link to={`/recipe/${recipe.id}/edit`}>Edit</Link>
+              {' | '}
+              <DeleteRecipeButton id={recipe.id} />
+            </li>
+          ))
+          }
+        </ul>
+   </div>
   );
 }
 
