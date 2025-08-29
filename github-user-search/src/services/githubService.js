729eeb0aa6c122c36701aac89cc7296511ac0dd6
Abdulmalik Abdullah <abdullahtemitope1@gmail.com>
2025-08-29 23:20:57 +0100

    corrections

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,19 +1,18 @@
-const API_KEY = import.meta.env.VITE_APP_GITHUB_API_KEY;
-const BASE_URL = import.meta.env.VITE_APP_API_URL;
+import axios from "axios";
 
+const BASE_URL = "https://api.github.com/users";
 
-async function fetchUserData(username) {
-    try {
-        const response = await fetch(`https://api.github.com/users/${username}`);
+export async function fetchUserData(username) {
+  try {
+    const response = await axios.get(`${BASE_URL}/${username}`);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching user data:", error);
 
-        if (!response.ok) {
-            throw new Error(`User not found: ${username}`);
-        }
-
-        const data = await response.json();
-        return data;
-    } catch (error) {
-        console.error("Error fetching user data:", error.message);
-        throw error;
-    }
+    throw new Error(
+      error.response?.status === 404
+        ? "User not found"
+        : "Failed to fetch user data"
+    );
+  }
 }
